36267a54f2ae1ee5c36da9699799fc9c2f0b4669
Liz Martinez <40329316+elizabethmv@users.noreply.github.com>
2019-08-23 15:59:31 -0500

    add code and test for entire solution

--- lib/fizzbuzz_elixir.ex
+++ lib/fizzbuzz_elixir.ex
@@ -1,13 +1,12 @@
 defmodule FizzbuzzElixir do
   def fizzbuzz do
-    list =
-      Enum.map(Enum.to_list(1..100), fn number ->
-        cond do
-          rem(number, 3) == 0 -> "fizz"
-          true -> number
-        end
-      end)
-
-    IO.inspect(list)
+    Enum.map(Enum.to_list(1..100), fn number ->
+      cond do
+        rem(number, 15) == 0 -> "fizzbuzz"
+        rem(number, 3) == 0 -> "fizz"
+        rem(number, 5) == 0 -> "buzz"
+        true -> number
+      end
+    end)
   end
 end
--- test/fizzbuzz_elixir_test.exs
+++ test/fizzbuzz_elixir_test.exs
@@ -10,4 +10,13 @@ defmodule FizzbuzzElixirTest do
     assert Enum.at(FizzbuzzElixir.fizzbuzz(), 2) == "fizz"
     assert Enum.at(FizzbuzzElixir.fizzbuzz(), 32) == "fizz"
   end
+
+  test "returns buzz instead of multiples of 5" do
+    assert Enum.at(FizzbuzzElixir.fizzbuzz(), 4) == "buzz"
+    assert Enum.at(FizzbuzzElixir.fizzbuzz(), 9) == "buzz"
+  end
+
+  test "returns fizzbuzz instead of multiples of 3 and 5" do
+    assert Enum.at(FizzbuzzElixir.fizzbuzz(), 14) == "fizzbuzz"
+  end
 end
